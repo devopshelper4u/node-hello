588592c7720a943a68d13f57b2725aac7295a1ad
Sunil <107870128+devopshelper4u@users.noreply.github.com>
2022-10-28 22:33:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello World3!\n'
+  const msg = 'Hello World4!\n'
   res.end(msg);
 });
 
